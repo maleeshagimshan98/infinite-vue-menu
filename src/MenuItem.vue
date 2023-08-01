9b94d1229f1533e4b93495be24b0451631f8dfd9
Maleesha Gimshan <47087449+maleeshagimshan98@users.noreply.github.com>
2023-08-01 12:51:14 +0530

    -removed multiple styling props and introduced a single prop for all styles -removed the router navigation from the component

--- src/MenuItem.vue
+++ src/MenuItem.vue
@@ -1,23 +1,35 @@
 /**
  * © Maleesha Gimshan - 2023 - github.com/maleeshagimshan98
- * Menu item
+ * Menu item 
  */
 
 <template>
-  <div class="" v-bind:class="[isActive ? [bgActive] : [bgInactive]]" :key="key" v-on:click="selected()">
+  <div
+    class=""
+    v-bind:class="[isActive ? [styles.item.active] : [styles.item.idle]]"
+    :key="id"
+    v-on:click="selected(id)">
     <!-- menu item content -->
     <slot>
-      <p class="" v-bind:class="[isActive ? [textActive] : [textInactive]]">
+      <p
+        class=""
+        v-bind:class="[isActive ? [styles.text.active] : [styles.text.idle]]">
         {{ title }}
       </p>
 
       <!-- children -->
       <div v-if="isActive && children">
-        <infinite-vue-menu-item v-for="(child, name, index) in children" :title="child.title" :key="child.key" :isActive="child.isActive" :children="child.children"
-          :bgActive="child.styles.bgActive" :bgHover="child.styles.bgHover" :bgInactive="child.styles.bgInactive"
-          :textActive="child.styles.textActive" :textHover="child.styles.textHover" :textInactive="child.styles.textInactive"
-          :styles="child.styles.font">
-        </infinite-vue-menu-item>>
+        <infinite-vue-menu-item
+          v-for="(child, name, index) in children"
+          :key="child.id"
+          :title="child.title"
+          :id="child.id"
+          :isActive="child.isActive"
+          :children="child.getChildren()"
+          :styles="child.getStyles()"
+          @menu:isActive="selected(child.id)"          
+          >
+        </infinite-vue-menu-item>
       </div>
     </slot>
   </div>
@@ -25,75 +37,37 @@
 
 <script>
 export default {
-  name: 'infinite-vue-menu-item',
+  name: "infinite-vue-menu-item",
   data: function () {
-    return {};
+    return {}
   },
   props: {
-    closeOnClick : {
-        type : Boolean,
-        default : true
+    closeOnClick: {
+      type: Boolean,
+      default: true,
     },
     title: {
       type: String,
       default: "",
     },
-    styles : {
-      type : [Array],
-    },
-    key: {
-      type: String
+    id: {
+      type: [String,Number],
     },
     isActive: {
       type: Boolean,
-      default: false
+      default: false,
     },
     children: {
-      type: Object
+      type: Object,
     },
-    bgActive: {
-      type: String,
-      default: "bg-dark"
-    },
-    bgHover: {
-      type: String,
-      default: "bg-secondary"
-    },
-    bgInactive: {
-      type: String,
-      default: "bg-white"
-    },
-    textActive: {
-      type: String,
-      default: "text-white"
-    },
-    textHover: {
-      type: String,
-      default: "bg-dark"
-    },
-    textInactive: {
-      type: String,
-      default: "text-secondary"
-    },
-    font: {
-      type: String,
-      default: ""
+    styles: {
+      type: [Object],
     },
   },
-  methods: {    
-    selected(child) {
-      // if (child) {
-      //   //... set the child isActive state
-      //   child.setActive(true)
-      //   this.route({path : child.path, routeName : child.routeName, params : child.params})
-      // }
-      // else {
-      //... set the active state
-      this.$emit('sidemenu:isActive')
-      this.route({ path: this.path, routeName: this.routeName, params: this.params })
-      this.$emit('sidemenu:navigate')
-      //}
-    }
+  methods: {
+    selected(id) {
+      this.$emit("menu:isActive",id)
+    },
   },
-};
+}
 </script>
