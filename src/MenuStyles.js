851a61ef5f5869335272a40f0e36566678d14439
Maleesha Gimshan <47087449+maleeshagimshan98@users.noreply.github.com>
2024-02-11 05:22:01 +0530

    -added seperate properties for states of the menu per each element -set the style values. Sets the default values if there are no explicitly passed values

--- src/MenuStyles.js
+++ src/MenuStyles.js
@@ -4,51 +4,82 @@
  */
 
 class MenuStyles {
-    
-    /**
-     * constructor
-     * 
-     * @param {Object}
-     * @returns {MenuStyles} 
-     */
-    constructor (item, text)
-    {
-        this._item = {}
-        this._text = {}
-        this.setStyles(item, text)        
+  /**
+   * constructor
+   *
+   * @param {Object}
+   * @returns {MenuStyles}
+   */
+  constructor(item, text) {
+    this._item = {
+      idle: [],
+      active: [],
+      disable: [],
     }
+    this._text = {
+      idle: [],
+      active: [],
+      disable: [],
+    }
+    this._setDefaultStyles()
+  }
 
-    /**
-     * get the style object
-     * 
-     * @returns {object} styles
-     */
-    getStyles ()
-    {
-        return {
-            item : this._item,
-            text : this._text
-        }
+  /**
+   * get the style object
+   *
+   * @returns {object} styles
+   */
+  getStyles() {
+    return {
+      item: this._item,
+      text: this._text,
     }
+  }
 
-    /**
-     * set styles,
-     * if any of property is not set, set it to default
-     * 
-     * @param {Object | null} item
-     * @param {Object | null} text
-     * @returns {object} 
-     */
-    setStyles (item, text)
-    {
-        let defaultStyles =  {
-            idle : [],
-            active : [],
-            disable : [],
-        }
-        this._item = item ?? defaultStyles
-        this._text = text ?? defaultStyles
+  /**
+   * Set default styles for the menu item
+   *
+   * @return {void}
+   */
+  _setDefaultStyles() {
+    let defaultStyles = {
+      idle: [],
+      active: [],
+      disable: [],
     }
+    this._item = defaultStyles
+    this._text = defaultStyles
+  }
+
+  /**
+   * Assign the style values to the style elements
+   * 
+   * @param {Object} styles
+   * @return {void}
+   */
+  _assignStyleValues (styles) {
+    let element = {}
+    ['idle', 'active', 'disable'].forEach (el => {
+        if (!styles.hasOwnProperty(el)) {
+        console.warn(`Infinite-Vue-Menu : Warning - Cannot set the styles. Styles object does not have a property ${el}`)
+        }
+        element[el] = styles[el] ?? []
+    })
+    return element
+  }
+
+  /**
+   * set styles,
+   * if any of property is not set, set it to default
+   *
+   * @param {object} item
+   * @param {object} text
+   * @returns {object}
+   */
+  setStyles(item, text) {
+    this._item = this._assignStyleValues(item)
+    this._text = this._assignStyleValues(text)
+  }
 }
 
-export default MenuStyles;
+export default MenuStyles
